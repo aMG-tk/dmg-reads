ecbf58e04998c3f1b5e334a412803b2e30457946
genomewalker <genomewalker@gmail.com>
2022-12-05 17:43:24 +0100

    Minor

--- dmg_reads/__main__.py
+++ dmg_reads/__main__.py
@@ -38,6 +38,9 @@ import tqdm
 from collections import defaultdict
 import re
 
+import cProfile as profile
+import pstats
+
 log = logging.getLogger("my_logger")
 
 
@@ -133,7 +136,8 @@ def main():
             log.info("Re-loading BAM file")
             samfile = pysam.AlignmentFile(bam, "rb")
     pysam.set_verbosity(save)
-
+    prof = profile.Profile()
+    prof.enable()
     refs_bam = [
         chrom.contig for chrom in samfile.get_index_statistics() if chrom.mapped > 0
     ]
@@ -242,6 +246,11 @@ def main():
             os.remove(out_files[file])
 
     logging.info("Done!")
+
+    prof.disable()
+    # print profiling output
+    stats = pstats.Stats(prof).strip_dirs().sort_stats("tottime")
+    stats.print_stats(10)  # top 10 rows
 
 
 if __name__ == "__main__":
