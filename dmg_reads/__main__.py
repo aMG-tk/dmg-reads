57478f8d103d0b3fa8354330eab72165c9f27fd3
genomewalker <genomewalker@gmail.com>
2022-12-06 17:42:10 +0100

    Minor

--- dmg_reads/__main__.py
+++ dmg_reads/__main__.py
@@ -84,6 +84,8 @@ def main():
 
         # get get_ranks
         ranks = {valid_ranks[x]: args.rank[x] for x in args.rank if x in valid_ranks}
+        print(ranks)
+        exit()
         # get refs that belong to this taxonomy
         # loop over all rows
         for i, row in tqdm.tqdm(
